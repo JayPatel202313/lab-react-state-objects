f325dd349053fe6f8bfbb9cbe4054c4df977ede0
JayPatel202313 <jpatel@pursuit.org>
2024-04-15 08:06:29 -0400

    COmplete

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,69 @@
+import React from 'react'
 import Footer from "./Footer";
 import Header from "./Header";
+import { useState } from 'react'
+import data from "./data";
 
 function App() {
+  console.log('App.js - data', data)
+
+
+  const [total, updateTotal] = useState(0); // change name
+  const [order, currentOrder] = useState([]);
+
+
+  function addOrder(item) {
+    currentOrder([...order, item]);
+    updateTotal(prevtotal => prevtotal + item.price);
+  }
+
+  function removeItem(item) {
+    currentOrder(order.slice(0, (order.indexOf(item))).concat(order.slice(order.indexOf(item) + 1)));
+    updateTotal(prevtotal => prevtotal - item.price);
+  }
+
+  function closeOut() {
+    currentOrder([]);
+    updateTotal(0);
+  }
+
+
   return (
     <div className="App">
       <Header />
       <main>
         <aside>
-          <table></table>
+          <table>
+            {data.map((item) => (
+              <tr key={item.id} onClick={() => addOrder(item)}>
+                <td>{item.image}</td>
+                <td className="item-name">
+                  <span>{item.name}</span><br />
+                  <span>{item.image.repeat(item.spiceLevel)}</span>
+                </td>
+                <td>${item.price}</td>
+              </tr>
+            ))}
+          </table>
+
         </aside>
         <section>
           <div>
             <h2>Current Order</h2>
-            <ul></ul>
-            <h4>Total:</h4>
+            <ul>
+              {order.map(item => {
+                return (
+                  <li>
+                    <span onClick={() => removeItem(item)}>X</span><span>{item.name}</span>${item.price}
+                  </li>
+                )
+              })}
+
+            </ul>
+            <h4>Total: ${total}</h4>
             <div>
-              <button>Tidy order</button>
-              <button>Close order</button>
+
+              <button onClick={closeOut}>Close order</button>
             </div>
           </div>
         </section>
